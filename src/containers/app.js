a96eba48ccf9aaf6f27a4267f1f78732f449d6e5
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-06-25 04:03:50 +0100

    consomation api

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import SearchBar from '../components/search-bar'
 import VideoList from './list-video'
 import axios from 'axios'
+import VideoDetail from '../components/video-details'
 
 const API_END_POINT = "https://api.themoviedb.org/3/"
 const POPULAR_MOVIES_URL = "discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images"
@@ -17,13 +18,13 @@ class App extends Component {
     componentWillMount(){
 
         axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
-
+                      
                     this.setState({movieList:response.data.results.slice(1,6)});
-                    this.setState({currentMovie:response.data.results[0]})
-                      console.log('===========');
-                     console.log('', this.state.movieList);
-                      console.log('===========');
+                    this.setState({currentMovie:response.data.results[1]});
 
+                    console.log({movieList:response.data.results});
+
+                   
         }.bind(this));
     }
 
@@ -32,6 +33,7 @@ class App extends Component {
         return <div>
             <SearchBar/>
             <VideoList/>
+            <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview}/>
             </div>
     }
  
